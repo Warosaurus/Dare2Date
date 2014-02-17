ca883cc3bff98b61c0b41cef91474f5eda4d791b
Warwick Louw <warwickbrettlouw@gmail.com>
2014-02-17 18:46:48 +0100

    Method for adding users

--- src/datefate/DateFate.java
+++ src/datefate/DateFate.java
@@ -3,22 +3,47 @@ import java.util.Scanner.*; //Temp for testing
 
 public class DateFate {
 
-	public User[] addUsers(){
-		int userNum = 1;
-		while()
-		User[] tempArr = new User[1];
+	public static User[] addUsers(){
+		java.util.Scanner scan = new java.util.Scanner(System.in);
+		int userNum = 0;
 		User[] useArr = new User[userNum];
-
-		//Place new users into the existing userArray
-		for (int i = 0; i < userNum;i++){
-			
+		char cont = 'y';
+		while(cont == 'y' || cont == 'Y'){
+			System.out.println("First Name:");
+			String newFName = scan.nextLine();
+			System.out.println("Last Name:");
+			String newLName = scan.nextLine();
+			System.out.println("Password:");
+			String newPass = scan.nextLine();
+			System.out.println("Age:");
+			int newAge = scan.nextInt();
+			User newUser = new User(newFName,newLName,newPass,newAge);
+			//Adapt the user array to fit the new user in.
+			if (userNum == 0){
+				useArr = new User[1];
+				useArr[0] = newUser;
+			} else {
+				User[] tempArr = new User[userNum + 1];
+				for (int i = 0; i < userNum - 1; i++){
+					tempArr[i] = useArr[i];
+				}
+				tempArr[userNum] = newUser;
+				useArr = new User[userNum + 1];
+				for (int i = 0; i < userNum; i++){
+					useArr[i] = tempArr[i];
+				}
+			}
+			userNum++;
+			System.out.print("Continue?");
+			cont = scan.next().charAt(0);
+			String emp = scan.nextLine();
 		}
 		return useArr;
 	}
 	
 	
     public static void main(String[] args) {
-		User user1 = new User("John","Paul","JP01",21);
-		
+		User[] users = addUsers();
+		System.out.println(users.length);
     }
 }
