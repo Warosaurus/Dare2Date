7fcc0ac584665692d5d4fb3c27e629c270153c9c
Warwick Louw <warwickbrettlouw@gmail.com>
2014-03-06 15:11:37 +0100

    Added Links, A few modifications

--- Dare2Date/src/Base/Response.java
+++ Dare2Date/src/Base/Response.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+	This is a generic response.
  */
 
 package Base;
@@ -11,5 +9,8 @@ package Base;
  * @author Indi
  */
 public class Response {
+	private Object response;
+	private String error;
+	
 	
 }
