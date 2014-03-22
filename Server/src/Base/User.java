8e7edf210dae6bfc346dcee534271ec8335a36c0
Warwick Louw <warwickbrettlouw@gmail.com>
2014-03-23 00:23:57 +0100

    pre-merge

--- Server/src/Base/User.java
+++ Server/src/Base/User.java
@@ -1,75 +1,205 @@
 package Base;
 
+import java.util.Calendar;
 import java.io.Serializable;
 
+/**
+ *
+ * 
+ */
 public class User implements Serializable {
+
 	private String fName;
 	private String lName;
+	private String gender;
 	private int userid;
 	private int level;
 	private int age;
+	private Calendar birthdate;
+	private String location;
 
+	/**
+	 *
+	 * Default constructor
+	 *
+	 * Default level of user is 1, for guests.
+	 * 
+	 */
 	public User() {
+		level = 1;
 	}
 
-	//Setter methods
-	
-	//set user id of user
-	public void setUserid(int userid) {
-		this.userid = userid;
-	}
-	
-	//set first name of user
-	public void setFName(String fName){
+	/**
+	 *
+	 * Constructor
+	 * @param fName
+	 * @param lName
+	 * @param gender
+	 * @param userid
+	 * @param level
+	 * @param age
+	 * @param birthdate
+	 * @param location
+	 */
+	public User(String fName, String lName, String gender, int userid, int level, int age, Calendar birthdate, String location) {
 		this.fName = fName;
-	}
-
-	//set Last name of user
-	public void setLName(String lName){
 		this.lName = lName;
-	}
-
-
-	//set age of user
-	public void setAge(int age){
-		this.age = age;
-	}
-
-	//set level of user
-	public void setLevel(int level){
+		this.gender = gender;
+		this.userid = userid;
 		this.level = level;
+		this.age = age;
+		this.birthdate = birthdate;
+		this.location = location;
 	}
-	
-	//Getter methods
-	
-	//get id of user
+
+	/**
+	 * Get the id of the user
+	 *
+	 * @return int
+	 */
 	public int getUserid() {
 		return userid;
 	}
 
-	//get first name of user
-	public String getFName(){
+	/**
+	 * Get the first name of the user
+	 *
+	 * @return String
+	 */
+	public String getFName() {
 		return this.fName;
 	}
 
-	//get last name of user
-	public String getLName(){
+	/**
+	 * Get the last name of the user
+	 *
+	 * @return String
+	 */
+	public String getLName() {
 		return this.lName;
 	}
 
-	//get age of user
-	public int getAge(){
+	/**
+	 * Get the gender of the user
+	 *
+	 * @return String
+	 */
+	public String getGender() {
+		return gender;
+	}
+
+	/**
+	 * Get the age of the user
+	 *
+	 * @return int
+	 */
+	public int getAge() {
 		return this.age;
 	}
 
-	//get level of user
+	/**
+	 * Get the birthdate of the user
+	 *
+	 * @return int
+	 */
+	public Calendar getBirthdate() {
+		return birthdate;
+	}
+
+	/**
+	 * Get the level of the user
+	 *
+	 * @return int
+	 */
 	public int getLevel() {
 		return level;
+	}
+	
+	/**
+	 * Get the location of the user
+	 *
+	 * @return String
+	 */
+	public String getLocation() {
+		return this.location;
+	}
+
+	/**
+	 * Set the user id of the user
+	 *
+	 * @param userid
+	 */
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+
+	/**
+	 * Set the first name of the user
+	 *
+	 * @param fName
+	 */
+	public void setFName(String fName) {
+		this.fName = fName;
+	}
+
+	/**
+	 * Set the Last name of the user
+	 *
+	 * @param lName
+	 */
+	public void setLName(String lName) {
+		this.lName = lName;
+	}
+
+	/**
+	 * Set the gender of the user
+	 *
+	 * @param gender
+	 */
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	/**
+	 * Set the age of the user
+	 *
+	 * @param age
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	/**
+	 * Set the birthdate of the user
+	 *
+	 * @param birthdate
+	 */
+	public void setBirthdate(Calendar birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	/**
+	 * Set the level of the user
+	 *
+	 * @param level
+	 */
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	
+	/**
+	 * Set the location of the user
+	 *
+	 * @param location
+	 */
+	public void setLocation(String location) {
+		this.location = location;
 	}
 
 	@Override
 	public String toString() {
 		return "User{" + "fName=" + fName + ", lName=" + lName + ", userid=" + userid + ", level=" + level + ", age=" + age + '}';
 	}
+
 
 }
--- Server/src/Server/DateServer.java
+++ Server/src/Server/DateServer.java
@@ -3,16 +3,33 @@ package Server;
 import Base.*;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
-import java.rmi.NotBoundException;
 import java.net.MalformedURLException;
 import java.rmi.registry.LocateRegistry;
 
+/**
+ *
+ *
+ */
 public class DateServer {
+
+	/**
+	 *
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Server initialzing..");
 		try {
+			/**
+			 * Create a local registry at port 1099.
+			 */
 			LocateRegistry.createRegistry(1099);
+			/**
+			 * Create local instance of Service Interface called service.
+			 */
 			ServiceInterface service = new ServerImpl();
+			/**
+			 * Set the alias of the registry to "DateServer".
+			 */
 			Naming.rebind("DateServer", service);
 			System.out.println("Server Ready.");
 		} catch (RemoteException ex) {
