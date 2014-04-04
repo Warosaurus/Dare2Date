3dfb9d9d2e0deae03f31aa4f51d3c31ec93057ee
Warwick Louw <warwickbrettlouw@gmail.com>
2014-04-04 16:15:14 +0200

    Changes

--- Server/src/Server/ServerImpl.java
+++ Server/src/Server/ServerImpl.java
@@ -430,8 +430,8 @@ public class ServerImpl extends UnicastRemoteObject implements ServiceInterface 
 	public void sendMail(Mail mail) {
 		try {
 			String ip = clientIps.get(mail.getReciever().getUserid());
-			ip = "127.0.0.1"; //Error in setting ip address. For me, setting Virtualbox ip address.
-			ClientChatInterface chat = (ClientChatInterface) Naming.lookup("rmi://" + ip + "/DateClient");
+			//ip = "127.0.0.1"; //Error in setting ip address. For me, setting Virtualbox ip address.
+			ClientChatInterface chat = (ClientChatInterface) Naming.lookup(ip);
 			chat.recieveMail(mail);
 		} catch (MalformedURLException | NotBoundException | RemoteException e) {
 			System.out.println(e);
